f42cf6dee5466866ee3faaded4f26260e925eefe
Felipe Campos <felipe.campos@al.resilia.com.br>
2025-02-11 09:02:13 -0300

    adicionando mais telas do frontend e criando rotas de busca e home

--- index.js
+++ index.js
@@ -14,10 +14,17 @@ app.use(
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 app.use("/public", express.static(path.join(__dirname, "public")));
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "/pages"));
 
 app.get("/", (req, res) => {
-	res.send("funcionando");
+	if (req.query.busca == null) {
+		res.render("home", {});
+	}
+	res.render("busca", {});
+});
+
+app.get("/:slug", (req, res) => {
+	res.send(req.params.slug);
 });
 
 app.listen(3000, () => {
