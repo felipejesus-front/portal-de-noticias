465003a5e5929490226fed0557c384bb7d100f21
felipejesus-front <felipe.campos@al.resilia.com.br>
2025-02-18 14:33:07 -0300

    ending projects front and server

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ const app = express();
 const Posts = require("./Posts.js");
 
 mongoose
-	.connect(
-		"mongodb+srv://felipecampos:q1CqrkQltFX8vbQy@api-portal-de-noticias.llg11.mongodb.net/portal_de_noticia_prod?retryWrites=true&w=majority&appName=Api-portal-de-noticias",
-		{ useNewUrlParser: true, useUnifiedTopology: true }
-	)
-	.then(() => {
-		console.log("conectado ao banco com sucesso");
-	})
-	.catch((err) => {
-		console.log("Conexão com Banco de Dados Falhou:");
-		console.log(err.message);
-	});
+  .connect(
+    "mongodb+srv://felipecampos:q1CqrkQltFX8vbQy@api-portal-de-noticias.llg11.mongodb.net/portal_de_noticia_prod?retryWrites=true&w=majority&appName=Api-portal-de-noticias",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("conectado ao banco com sucesso");
+  })
+  .catch((err) => {
+    console.log("Conexão com Banco de Dados Falhou:");
+    console.log(err.message);
+  });
 
 app.use(bodyParser.json());
 app.use(
-	bodyParser.urlencoded({
-		extends: true,
-	})
+  bodyParser.urlencoded({
+    extends: true,
+  })
 );
 
 app.engine("html", require("ejs").renderFile);
@@ -33,77 +33,93 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "/pages"));
 
 app.get("/", (req, res) => {
-	if (req.query.busca == null) {
-		Posts.find({})
-			.sort({ _id: -1 })
-			.exec((err, posts) => {
-				posts = posts.map((post) => {
-					return {
-						titulo: post.titulo,
-						imagem: post.imagem,
-						categoria: post.categoria,
-						conteudo: post.conteudo,
-						descricaoCurta: post.conteudo.substring(0, 120),
-						slug: post.slug,
-					};
-				});
+  if (req.query.busca == null) {
+    Posts.find({})
+      .sort({ _id: -1 })
+      .exec((err, posts) => {
+        posts = posts.map((post) => {
+          return {
+            titulo: post.titulo,
+            imagem: post.imagem,
+            categoria: post.categoria,
+            conteudo: post.conteudo,
+            descricaoCurta: post.conteudo.substring(0, 120),
+            slug: post.slug,
+          };
+        });
 
-				Posts.find({})
-					.sort({ views: -1 })
-					.limit(5)
-					.exec((err, topPosts) => {
-						topPosts = topPosts.map((post) => {
-							return {
-								titulo: post.titulo,
-								imagem: post.imagem,
-								categoria: post.categoria,
-								conteudo: post.conteudo,
-								descricaoCurta: post.conteudo.substring(0, 120),
-								slug: post.slug,
-								views: post.views,
-							};
-						});
+        Posts.find({})
+          .sort({ views: -1 })
+          .limit(5)
+          .exec((err, topPosts) => {
+            topPosts = topPosts.map((post) => {
+              return {
+                titulo: post.titulo,
+                imagem: post.imagem,
+                categoria: post.categoria,
+                conteudo: post.conteudo,
+                descricaoCurta: post.conteudo.substring(0, 120),
+                slug: post.slug,
+                views: post.views,
+              };
+            });
 
-						res.render("home", { posts, topPosts });
-					});
-			});
-	} else {
-		res.render("busca", {});
-	}
+            res.render("home", { posts, topPosts });
+          });
+      });
+  } else {
+    Posts.find(
+      { titulo: { $regex: req.query.busca, $options: "i" } },
+      (err, posts) => {
+        posts = posts.map((post) => {
+          return {
+            titulo: post.titulo,
+            imagem: post.imagem,
+            categoria: post.categoria,
+            conteudo: post.conteudo,
+            descricaoCurta: post.conteudo.substring(0, 120),
+            slug: post.slug,
+          };
+        });
+
+        res.render("busca", { posts, contagem: posts.length });
+      }
+    );
+  }
 });
 
 app.get("/:slug", (req, res) => {
-	Posts.findOneAndUpdate(
-		{ slug: req.params.slug },
-		{ $inc: { views: 1 } },
-		{ new: true },
-		(err, post) => {
-			if (post != null) {
-				Posts.find({})
-					.sort({ views: -1 })
-					.limit(5)
-					.exec((err, topPosts) => {
-						topPosts = topPosts.map((post) => {
-							return {
-								titulo: post.titulo,
-								imagem: post.imagem,
-								categoria: post.categoria,
-								conteudo: post.conteudo,
-								descricaoCurta: post.conteudo.substring(0, 120),
-								slug: post.slug,
-								views: post.views,
-							};
-						});
+  Posts.findOneAndUpdate(
+    { slug: req.params.slug },
+    { $inc: { views: 1 } },
+    { new: true },
+    (err, post) => {
+      if (post != null) {
+        Posts.find({})
+          .sort({ views: -1 })
+          .limit(5)
+          .exec((err, topPosts) => {
+            topPosts = topPosts.map((post) => {
+              return {
+                titulo: post.titulo,
+                imagem: post.imagem,
+                categoria: post.categoria,
+                conteudo: post.conteudo,
+                descricaoCurta: post.conteudo.substring(0, 120),
+                slug: post.slug,
+                views: post.views,
+              };
+            });
 
-						res.render("single", { post, topPosts });
-					});
-			} else {
-				res.redirect("/");
-			}
-		}
-	);
+            res.render("single", { post, topPosts });
+          });
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
 });
 
 app.listen(3000, () => {
-	console.log("Server Rodando...");
+  console.log("Server Rodando...");
 });
